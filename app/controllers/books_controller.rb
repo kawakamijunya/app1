052e3b7eff46dcb642578a77f3836d6eee42f96d
EC2 Default User <ec2-user@ip-172-31-45-76.ap-northeast-1.compute.internal>
2022-12-06 05:55:12 +0000

    [Add]3-13

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,21 +1,21 @@
 class BooksController < ApplicationController
-  def new
-    @book=Book.new
-  end
 
   def create
-   book=Book.new(book_params)
-   if book.save
+   @book=Book.new(book_params)
+   if @book.save
    flash[:notice]="Successfully created"
-    redirect_to books_path(@book.id) #投稿一覧へリダイレクト
+    redirect_to book_path(@book.id)
+           #idはインスタンス変数
    else
-    render :new
+     @books=Book.all
+    render :index
    end
   end
 
 
   def index
     @books=Book.all
+    @book=Book.new
   end
 
   def show
@@ -27,18 +27,19 @@ class BooksController < ApplicationController
   end
 
   def update
-    book=Book.find(params[:id])
-    if book.update(book_params)
+    @book=Book.find(params[:id])
+    if @book.update(book_params)
      flash[:notice]="Successfully updated"
-     redirect_to book_path(book.id) #詳細画面へリダイレクト
+     redirect_to book_path(@book.id) #詳細画面へリダイレクト
+               #idはインスタンス変数
     else
-       render :new
+       render :edit
     end
   end
 
   def destroy
-    book=Book.find(params[:id]) #データを一件取得
-    book.destroy #データを削除
+    @book=Book.find(params[:id]) #データを一件取得
+    @book.destroy #データを削除
     flash[:notice] = "Successfully deleted"
     redirect_to '/books' #投稿一覧画面へリダイレクト
   end
